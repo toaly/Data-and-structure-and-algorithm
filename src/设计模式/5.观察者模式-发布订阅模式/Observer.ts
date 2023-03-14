80c3c3c2a582a8e57ced405fcd50f838afb96f2e
Aly <825437313@qq.com>
2023-03-14 08:09:38 +0800

    添加一些图片

--- src/设计模式/5.观察者模式-发布订阅模式/Observer.ts
+++ src/设计模式/5.观察者模式-发布订阅模式/Observer.ts
@@ -52,5 +52,99 @@ Observer.addObserver(obj2)
 Observer.notify()
 
 
+
+interface IMessage {
+    [key: string]: any[];
+}
+
+type Listener = (content: any) => void;
+
+class PubSub {
+    private messages: IMessage = {};
+    private listeners: { [key: string]: Listener[] } = {};
+
+    public publish(type: string, content: any): void {
+        const existContent = this.messages[type];
+        if (!existContent) {
+            this.messages[type] = [];
+        }
+        this.messages[type].push(content);
+    }
+
+    public subscribe(type: string, cb: Listener): void {
+        const existListener = this.listeners[type];
+        if (!existListener) {
+            this.listeners[type] = [];
+        }
+        this.listeners[type].push(cb);
+    }
+
+    public notify(type: string): void {
+        const messages = this.messages[type];
+        const subscribers = this.listeners[type] || [];
+        subscribers.forEach((cb, index) => cb(messages[index]));
+    }
+}
+
+class Publisher {
+    private name: string;
+    private context: PubSub;
+
+    constructor(name: string, context: PubSub) {
+        this.name = name;
+        this.context = context;
+    }
+
+    public publish(type: string, content: any): void {
+        this.context.publish(type, content);
+    }
+}
+
+class Subscriber {
+    private name: string;
+    private context: PubSub;
+
+    constructor(name: string, context: PubSub) {
+        this.name = name;
+        this.context = context;
+    }
+
+    public subscribe(type: string, cb: Listener): void {
+        this.context.subscribe(type, cb);
+    }
+}
+
+const TYPE_A = 'music';
+const TYPE_B = 'movie';
+const TYPE_C = 'novel';
+
+const pubsub = new PubSub();
+
+const publisherA = new Publisher('publisherA', pubsub);
+publisherA.publish(TYPE_A, 'we are young');
+publisherA.publish(TYPE_B, 'the silicon valley');
+const publisherB = new Publisher('publisherB', pubsub);
+publisherB.publish(TYPE_A, 'stronger');
+const publisherC = new Publisher('publisherC', pubsub);
+publisherC.publish(TYPE_C, 'a brief history of time');
+
+const subscriberA = new Subscriber('subscriberA', pubsub);
+subscriberA.subscribe(TYPE_A, res => {
+    console.log('subscriberA received', res)
+});
+const subscriberB = new Subscriber('subscriberB', pubsub);
+subscriberB.subscribe(TYPE_C, res => {
+    console.log('subscriberB received', res)
+});
+const subscriberC = new Subscriber('subscriberC', pubsub);
+subscriberC.subscribe(TYPE_B, res => {
+    console.log('subscriberC received', res)
+});
+
+pubsub.notify(TYPE_A);
+pubsub.notify(TYPE_B);
+pubsub.notify(TYPE_C);
+
+
 // 发布-订阅模式
 
